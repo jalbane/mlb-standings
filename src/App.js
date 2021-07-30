b356e92403ce6bce56e8dfd7fe896da38b71aad3
Jeff Albanese <jalbane9@gmail.com>
2021-07-30 13:14:06 -0400

    media queries and component changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
+<<<<<<< HEAD
 import League from './League';
 {/*import Mlb from './components/Mlb'*/}
 
+=======
+import League from './components/League'
+>>>>>>> parent of f9b97df (Updates)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
        <h3> MLB Standings </h3>
         {/*<Mlb />*/}
         <League league={0}/>
         <League league={1}/>
+=======
+       <h2> MLB Standings </h2>
+        <League />
+        <br></br>
+>>>>>>> parent of f9b97df (Updates)
     </div>
   );
 }
